fe7517870f8db797d28e2b3052b8d9db1bea0b66
Jason Jhonson <jjhonson@github.com>
2016-05-10 09:13:37 -0700

    Adding first js exercise

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("How old are you?");
+alert("Your name is " + firstName + " " + lastName);
+alert("and you are " + age + " years old!!");
+var days = age * 365;
+alert(age + " year is roughly" + days + " days!");
