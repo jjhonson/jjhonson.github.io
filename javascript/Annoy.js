29c0b3c7ca100950d0f0fe8a62c6d2230b0cc191
Jason Jhonson <jjhonson@github.com>
2016-05-11 13:31:05 -0700

    Add Annoy-o-matic

---
+++ javascript/Annoy.js
@@ -0,0 +1,9 @@
+var answer = prompt("Are we there yet?");
+
+while(answer !== "yes" && answer !== "yeah") {
+  var answer = prompt("Are we there yet?");
+}
+
+if(answer +++ "yes") {
+  alert("Yay, we made it!!")
+}
