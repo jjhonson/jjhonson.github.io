49c76613fe950ab9573e0f73e9551c616568d22d
Jason Jhonson <jjhonson@github.com>
2016-05-11 14:18:02 -0700

    Upgrade annoyomatic to v2

--- javascript/Annoy.js
+++ javascript/Annoy.js
@@ -1,6 +1,18 @@
+// var answer = prompt("Are we there yet?");
+//
+// while(answer !== "yes" && answer !== "yeah") {
+//   var answer = prompt("Are we there yet?");
+// }
+//
+// if(answer +++ "yes") {
+//   alert("Yay, we made it!!")
+// }
+
+// VERSION 2 - include indexOf so that any answer with the right answer in it works
+
 var answer = prompt("Are we there yet?");
 
-while(answer !== "yes" && answer !== "yeah") {
+while(answer.indexOf("yes") && answer.indexOf("yeah") ===-1) {
   var answer = prompt("Are we there yet?");
 }
 
