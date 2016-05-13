74d8fa075c8d1e591c41437f0b9cbe2ffad39b3d
Jason Jhonson <jjhonson@github.com>
2016-05-13 15:17:23 -0700

    add function exercise

--- javascript/function.js
+++ javascript/function.js
@@ -1,10 +1,32 @@
 function isEven(num) {
   //return true if even
-  if(num % 2 === 0) {
-    return true;
+  return (num % 2 === 0);
+}
+//     return true;
+//   }
+//   //return false if otherwise
+//   else {
+//     return false;
+//   }
+// }
+
+
+function factorial(num){
+  // define a result variable
+  var result = 1;
+  //calculate factorial and store value in result
+  for(var i = 2; i <= num; i++) {
+    result *= i;
   }
-  //return false if otherwise
-  else {
-    return false;
-  }
+  // return a result variable
+  return result;
+}
+// factorial  4 x 3 x 2 x 1
+
+
+function kebabToSnake(str){
+  //replace all dashes with underscores
+  var newStr = str.replace(/-/g , "_");
+  //return str
+  return newStr;
 }
