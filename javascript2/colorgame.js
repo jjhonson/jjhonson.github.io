8a9252b0981f21e6d6f76a9443c347719b3122b6
Jason Jhonson <jjhonson@github.com>
2016-05-22 11:13:55 -0700

    updating progress on color game

--- javascript2/colorgame.js
+++ javascript2/colorgame.js
@@ -1,4 +1,4 @@
-var color = [
+var colors = [
   "rgb(255, 0, 0)",
   "rgb(255, 255, 0)",
   "rgb(0, 255, 0)",
@@ -7,11 +7,35 @@ var color = [
   "rgb(255, 0, 255)",
 ]
 var squares = document.querySelectorAll(".square");
-var pickedColor = color[3];
+var pickedColor = colors[3];
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
-  squares[i].style.background = color[i];
+  //add initial colors to squares
+  squares[i].style.background = colors[i];
+
+  //add click listeners to squares
+  squares[i].addEventListener("click", function(){
+    //grab color of clicked square
+    var clickedColor = this.style.background;
+    //compare color to pickedColor
+    if (clickedColor === pickedColor){
+      messageDisplay.textContent = "Correct!";
+      changeColors(clickedColor);
+    } else{
+      this.style.background = "#232323";
+      messageDisplay.textContent = "Try Again";
+    }
+  });
+}
+
+function changeColors(color){
+  //loop through all squares
+  for (var i = 0; i < squares.length; i++) {
+    //change each color to match given color
+    squares[i].style.background = color;
+  }
 }
