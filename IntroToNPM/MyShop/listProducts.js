6aca1b7b53646d1721ec15894431fbc03c91f39e
Jason Jhonson <jasonsjhonson@gmail.com>
2016-05-31 20:14:39 +0000

    Adding IntroToNPM file

---
+++ IntroToNPM/MyShop/listProducts.js
@@ -0,0 +1,9 @@
+var faker = require('faker');
+
+console.log("===================");
+console.log("Welcome to my Shop!");
+console.log("===================");
+
+ for (var i = 0; i < 10; i++) {
+    console.log (faker.fake("{{commerce.productName}} - ${{commerce.price}}"));
+ }
