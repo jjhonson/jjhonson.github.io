983c4d8e4eb8a49a69e72d8d34df76d15da25d31
Jason Jhonson <jasonsjhonson@gmail.com>
2016-06-01 18:39:00 +0000

    Updating the IntroTo Express files from C9

--- IntroToExpress/FirstExpressApp/app.js
+++ IntroToExpress/FirstExpressApp/app.js
@@ -13,6 +13,17 @@ app.get("/dog", function(req,res){
     res.send("Meow!!");
 });
 
+// a : creates a pattern
+app.get("/r/:sebredditName", function(req, res){
+    var subreddit = req.params.sebredditName;
+    res.send("WELCOME TO THE " + subreddit.toUpperCase()   + " SUBREDDIT!");
+});
+
+// a star will match all routes - needs to be at the bottom - order matters
+app.get("*", function(req,res){
+    res.send("You are a star!!");
+});
+
 // this is the port you need to listen on for Cloud9
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server has started!!");
